1dba489f8bd71850f4de25a32821663e1c74f5f4
kireledan <erik.nadel@gmail.com>
2020-08-24 20:01:26 -0700

    Update DB for questions

--- db/table.sql
+++ db/table.sql
@@ -2,7 +2,7 @@ CREATE TABLE connected_users
 		(
 		  chatid bigint NOT NULL,
 		  nextquestiontime timestamp with time zone,
-		  questioninterval INTERVAL
+		  questioninterval INTERVAL,
 		);
 		
 ALTER TABLE connected_users ADD CONSTRAINT chatid PRIMARY KEY (chatid);
@@ -16,3 +16,28 @@ ADD COLUMN questionsCorrect int;
 
 ALTER TABLE connected_users
 ADD COLUMN questionsSent int;
+
+ALTER TABLE connected_users
+ADD COLUMN QuizSection TEXT;
+UPDATE connected_users SET quizsection = 'network+'; 
+
+
+ALTER TABLE connected_users
+ADD COLUMN LastAnsweredTime timestamp with time zone;
+UPDATE connected_users SET LastAnsweredTime = ; 
+
+ALTER TABLE connected_users
+ADD COLUMN Username TEXT;
+UPDATE connected_users SET username = 'unknown'; 
+
+
+CREATE TABLE Questions(
+    QuestionText    text primary key,
+    Choices    		text[],
+    Answers           int[],
+	ID				text,
+	Tags			text[]
+);
+
+ALTER TABLE connected_users
+ADD COLUMN QuestionsAsked text[];
